c53da42ae934b7b1492a04f96346ced0a18c3d90
vignesh GJ <hackervignesh5@gmail.com>
2023-06-25 00:27:26 +0530

    Integrated chat GPT, but havent tested the model since doesnt have API key to test it

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
+import time
+
 import streamlit as st
 from PIL import Image
 import pandas as pd
 import numpy as np
+
+import constants
 from preprocessing_module import preprocessing
 from constants import project_list
 import utility_functions
+import LLM.model as model
 
+# c1, c2 = st.columns([2, 2])
+# c2.title('Welcome to Virtual Subject Matter Expert App (POC)')
 logo = Image.open('images/gen_ai.png')
-#st.set_page_config(layout='wide')
-st.subheader('Welcome to Virtual Subject matter expert POC App')
+# c1.image(logo)
+st.set_page_config(layout='wide')
 
+st.subheader('Welcome to Virtual Subject Matter Expert App (POC)')
 st.image(logo, width=300)
 st.divider()
 
@@ -18,17 +26,6 @@ col1.metric('Margins app development project', '80%', '2%')
 col2.metric('React UI for pharma client project', '90%', '-8%')
 col3.metric('Mobile app for grocery store project', '40%', '4%')
 col4.metric('New project placeholder', '-0%', '-0%')
-
-
-# Selectors
-# Radio button
-# genre = st.radio(
-#     "Please select your project",
-#     ('Margins app development', 'React UI for pharma client', 'Mobile app for grocery store')
-# )
-#
-# if genre == 'Margins app development':
-#     st.write('You have selected')
 
 
 # Select box
@@ -42,11 +39,15 @@ user_input = st.text_input('Please enter the query about project',
                            placeholder='example: What are all pending development Jira task in our project')
 
 
-if st.button('Submit'):
+if st.button('Get Answer'):
     if user_input:
-        text = 'Querying info about ' + str(option) + ' Project'
-        project_data = utility_functions.get_project_meta_data(option)
-        # Initialize custom pre-processor module
-        pp = preprocessing.Preprocess()
-        processed_project_meta_data = pp.pre_preprocess_text(project_data)
-        st.write(processed_project_meta_data)
+        text = 'Querying info about ' + str(option) + ' ....'
+        with st.spinner(text):
+            project_data = utility_functions.get_project_meta_data(option)
+            # Initialize custom pre-processor module
+            pp = preprocessing.Preprocess()
+            processed_project_meta_data = pp.pre_preprocess_text(project_data)
+            processed_project_meta_data_str = ''.join(processed_project_meta_data)
+            st.write(constants.base_prompt+processed_project_meta_data_str)
+            # model_answer = model.answer(constants.base_prompt+processed_project_meta_data_str, user_input)
+            # st.write(model_answer)
--- constants.py
+++ constants.py
@@ -9,3 +9,11 @@ project_meta_data_map = {
     'React UI for pharma client project': 'project_meta_data/react_ui_for_phrama_client_project_meta_data.txt',
     'Mobile app for grocery store project': 'project_meta_data/mobile_app_for_grocery_store_project_meta_data.txt'
 }
+
+base_prompt = "You are the Subject Matter Expert (SME) for a project and " \
+              "your role is to provide understanding and clarification regarding " \
+              "the project details. Users will ask you questions related to the project," \
+              " and you will act as their guide and source of information." \
+              " Your goal is to provide clear and accurate responses" \
+              " to help users understand the project better. Keep in mind" \
+              " that the below project details \n\n"
